b226f824005c12ddb43b632b39954e8fd2ba0405
Ugo Popée <github@p0p.ee>
2022-08-15 23:46:47 +0200

    Fixed syntax errors

--- Casks/defaults-edit.rb
+++ Casks/defaults-edit.rb
@@ -1,16 +1,10 @@
 cask "defaults-edit" do
 
-  module Utils
-    def self.semver
-        version.gsub(/_\d+/, '') 
-    end
-  end
-  
   version "1.4.5_1"
 
   sha256 "9ee9ebffb4f9d07450f901502d10a48e82b5f01f6a47d1ca462ad17e19c64373"
 
-  url "https://github.com/ThatsJustCheesy/defaults-edit/releases/download/v#{version}/defaults-edit.v#{Utils.semver}.zip"
+  url "https://github.com/ThatsJustCheesy/defaults-edit/releases/download/v#{version}/defaults-edit.v#{version.sub(%r{_\d+}, '')}.zip"
   name "Defaults edit"
   desc "Graphical user defaults editor for macOS"
   homepage "https://github.com/ThatsJustCheesy/defaults-edit"

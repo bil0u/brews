8ff3fd5b6f18c465f6641bd3d4a1009ffcf08b78
Ugo Popée <github@p0p.ee>
2022-08-15 23:32:39 +0200

    Updated cask

--- Casks/defaults-edit.rb
+++ Casks/defaults-edit.rb
@@ -1,9 +1,16 @@
 cask "defaults-edit" do
-  version "1.4.5"
-  suffix "_1"
+
+  module Utils
+    def self.semver
+        version.gsub(/_\d+/, '') 
+    end
+  end
+  
+  version "1.4.5_1"
+
   sha256 "9ee9ebffb4f9d07450f901502d10a48e82b5f01f6a47d1ca462ad17e19c64373"
 
-  url "https://github.com/ThatsJustCheesy/defaults-edit/releases/download/v#{version}#{suffix}/defaults-edit.v#{version}.zip"
+  url "https://github.com/ThatsJustCheesy/defaults-edit/releases/download/v#{version}/defaults-edit.v#{Utils.semver}.zip"
   name "Defaults edit"
   desc "Graphical user defaults editor for macOS"
   homepage "https://github.com/ThatsJustCheesy/defaults-edit"
